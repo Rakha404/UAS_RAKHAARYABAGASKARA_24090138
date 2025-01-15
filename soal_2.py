68fdf8c791a34ac3e6d0bf0c52cd96ec5d3673fc
Rakha404 <dawgsyogs@gmail.com>
2025-01-15 12:14:44 +0700

    Add files via upload

--- soal_2.py
+++ soal_2.py
@@ -1,3 +1,5 @@
+import pandas as p
+
 nilai = [
     [90,80],
     [50,60],
@@ -7,23 +9,9 @@ nilai = [
 index = ['Mahasiswa 1','Mahasiswwa 2', 'Mahasiswa 3'],
 columns= ['Nama','Algoritma dan Struktur Data 2','Matematika Numerik']
 
-def hitung_rata_rata(nilai):
-    rata_mk = [sum(col) / len(col) for col in zip(*nilai)]
-    rata_mhs = [sum(row) / len(row) for row in nilai]
-    return rata_mk, rata_mhs
+mean = nilai.mean(axis=0)
+rata = nilai.mean(axis=1)
 
-def hasil(nilai):
-    print("Tabel Nilai:")
-    for i, baris in enumerate(nilai, start=1):
-        print(f"Mahasiswa {i}: {baris}")
-
-    rata_mk, rata_mhs = hitung_rata_rata(nilai)
-    print("Rata-rata per Mata Kuliah:")
-    for i, rata in enumerate(rata_mk, start=1):
-        print(f"Mata Kuliah {i}: {rata:.2f}")
-
-    print("Rata-rata per Mahasiswa:")
-    for i, rata in enumerate(rata_mhs, start=1):
-        print(f"Mahasiswa {i}: {rata:.2f}")
-
-hasil(nilai)
+print (nilai)
+print(mean)
+print(rata)
